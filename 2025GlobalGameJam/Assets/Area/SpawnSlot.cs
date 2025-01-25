cbc3499cc1c20ab4e25d40b05d2653eec92727cc
Miabread <miabreadloaf@gmail.com>
2025-01-25 14:38:47 -0500

    Add ocean sky transitiion to area

--- 2025GlobalGameJam/Assets/Area/SpawnSlot.cs
+++ 2025GlobalGameJam/Assets/Area/SpawnSlot.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SpawnSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // This is low key just a marker script, idk man it works
 }
