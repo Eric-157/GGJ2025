ab39bd2b7e9b6f9fd0a0a88265d0d4f611a0a7c5
Miabread <miabreadloaf@gmail.com>
2025-01-25 16:55:06 -0500

    start name input stuff

--- 2025GlobalGameJam/Assets/Scoring/NameInput.cs
+++ 2025GlobalGameJam/Assets/Scoring/NameInput.cs
@@ -1,8 +1,59 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NameInput : MonoBehaviour
 {
+    public GameObject cursor;
 
+    public List<TMP_Text> letters;
+
+    private int selected = 0;
+
+    public void Start()
+    {
+        // Otherwise things get funky
+        MoveCursor();
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            selected += (int)Input.GetAxisRaw("Horizontal");
+
+            // Wrap around array bounds
+            if (selected < 0) selected = letters.Count - 1;
+            if (selected >= letters.Count) selected = 0;
+
+            MoveCursor();
+        }
+
+        if (Input.GetButtonDown("Vertical"))
+        {
+            // Evil and intimidating horse
+            char fuck = (char)(letters[selected].text[0] + (int)Input.GetAxisRaw("Vertical"));
+
+            // Wrap around the capitals
+            if (fuck < 'A') fuck = 'Z';
+            if (fuck > 'Z') fuck = 'A';
+
+            letters[selected].text = fuck.ToString();
+        }
+    }
+
+    public void MoveCursor()
+    {
+        // Who cares about parent transforms, this is way easier
+        cursor.transform.position = letters[selected].transform.position;
+    }
+
+    public string Name()
+    {
+        // Awesome lesbian couple
+        return String.Join("", from letter in letters select letter.text);
+    }
 }
