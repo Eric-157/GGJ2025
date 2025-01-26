3faa557b73c2c1632c86baf45aa4e451138fb324
N000L <nolancarey64@gmail.com>
2025-01-26 00:08:41 -0500

    enemies n stuff

--- 2025GlobalGameJam/Assets/Player/PlayerController.cs
+++ 2025GlobalGameJam/Assets/Player/PlayerController.cs
@@ -13,26 +13,30 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider2D;
     private SpriteRenderer spriteRenderer;
-    private float xAxis = 0;
+    private float xAxis;
+    private float yAxis;
     public bool canBeDamaged = true;
+    public int r, b, g;
 
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider2D = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        health = maxHealth;
+        health = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(xAxis * speed, rb.velocity.y);
+        rb.velocity = new Vector2(xAxis * speed, yAxis * speed);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -42,6 +46,11 @@ public class PlayerController : MonoBehaviour
         if (entityController.goodTag == "Pickup")
         {
             Destroy(collider.gameObject);
+            if (health < maxHealth)
+            {
+                health++;
+
+            }
         }
         if (entityController.goodTag == "Enemy" && canBeDamaged)
         {
@@ -49,11 +58,14 @@ public class PlayerController : MonoBehaviour
             canBeDamaged = false;
             StartCoroutine("DamageInvuln");
         }
+        float bubbleSize = 0.25f + (health / 10);
+        boxCollider2D.size = new Vector2(bubbleSize / 2, bubbleSize / 2);
+        spriteRenderer.transform.localScale = new Vector3(bubbleSize, bubbleSize, bubbleSize);
     }
 
     IEnumerator DamageInvuln()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         canBeDamaged = true;
     }
 }
